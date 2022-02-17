60a62ce92c5db4843117ddd18dcef4548860228f
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-02-17 16:17:08 -0300

    Exercício de Funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,99 @@
+//Exercício de Interpretação de Código
+//1.a)Os números da variavél multiplicados por 5.
+//  b)Nada
+//2.a)Essa função retorna true ou false dependendo do texto
+//    que o usuário inserir e deixa todas as letras minúsculas.
+//  b)I.eu gosto de cenoura \\ true
+//    II.cenoura é bom pra vista \\ true
+//    III. cenoura cresce na terra \\ true
+
+//Exercício de Escrita de Código 
+//1.a)
+
+// function saudacao() {
+//     const saudacao = "Meu nome é Wesley, moro no RJ e sou estudante."
+//     return saudacao
+// }
+
+// const mostrarLog1 = saudacao()
+// console.log(mostrarLog1)
+
+//  b)
+// function infPessoa(nome, idade, cidade, profissao) {
+//     const infPessoa = `"Eu sou ${nome}, tenho ${idade}, moro em ${cidade} e sou ${profissao}"." `
+//     return infPessoa
+// }
+
+// console.log(infPessoa("Alguém", "20", "Não Importa", "Qualquer Coisa"))
+
+//2.a)
+// function doisNum(x, y){
+//     const doisNum = x + y;
+    
+//     return doisNum
+// }
+
+// console.log(doisNum(5, 6));
+// console.log(doisNum(17, 3));
+//b)
+// function doisNum2(x, y){
+//     const doisNum2 = x >= y
+
+//     return doisNum2
+// }
+// console.log(doisNum2(4, 10));
+// console.log(doisNum2(10, 4));
+//c)
+// function umNum(x){
+//     const umNum = x % 2
+    
+//     return umNum
+// }
+
+// console.log(umNum(5));
+// console.log(umNum(6));
+//d)
+// function recUmaString(){
+//     const recUmaString = "Mensagem String"
+
+//     return recUmaString
+// }
+
+//     console.log(recUmaString() ,recUmaString.length);
+//3.
+// function operacoes1(x, y){
+//     const soma = x + y
+   
+//     return soma
+
+//     }
+
+
+// function operacoes2(x, y){
+//     const subtracao = x - y
+    
+//     return subtracao
+
+
+//     }
+    
+
+// function operacoes3(x, y){
+//         const multiplicacao = x * y
+       
+//         return multiplicacao
+    
+//         }
+    
+// function operacoes4(x, y){
+//          const divisao = x + y
+           
+//         return divisao
+        
+//         }
+        
+//         const x = Number(prompt("Insíra um número:"))
+//         const y = Number(prompt("Insíra outro número:"))
+//         console.log(operacoes1(x, y), operacoes2(x, y), operacoes3(x, y), operacoes4(x, y));
+        
+
