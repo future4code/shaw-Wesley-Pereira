3b1cc6482b3ad7eb3ae1f7d5b5235493d7dc5d06
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-02-21 15:26:07 -0300

    Exercicio de Objetos

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -1,4 +1,15 @@
 //Exercício de Interpretação de Código
-//1."Matheus Nachtergaele"
-//  "Matheus Nachtergaele"
-//  Canal Brasil 19h.
+//1.Matheus Nachtergaele
+//  Virginia Cavendish
+//  canal: "Globo", horario: "14h"
+
+//2.a)Objeto {nome: "Juca", idade: 3, raca: "SRD"}
+//    Objeto {nome: "Juba", idade: 3, raca: "SRD"}
+//    Objeto {nome: "Jobo", idade: 3, raca: "SRD"}
+//  b)Ela adiciona elementos dentro de um objeto matriz.
+//3.a)false, undefined
+//  b)É impresso false pois a função faz o elemento do objeto ser impresso
+//    Já o undefined é impresso porque o objeto nao contém o elemento altura.
+
+//Exercício de Escrita de Código
+//1.
