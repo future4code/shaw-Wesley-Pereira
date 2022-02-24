6535d8c5bc8368992dcc9820a2ec7525064529dc
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-02-24 16:38:05 -0300

    Atualizção

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -5,3 +5,27 @@
 
 //Exercício de Escrita de Código
 // 1.a)
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+//  const novosNomes = pets.map((pet) => { 
+//     console.log(pet.nome)
+     
+//  })
+ 
+// const racaSalcicha = pets.filter((pet) =>{
+//     return pet.raca === "Salsicha"
+// })
+// console.log(racaSalcicha)
+
+// const mensagemDesconto = pets.map((pet) => {
+//     console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${pet.nome}!`)
+// })
+
+//2.a)
