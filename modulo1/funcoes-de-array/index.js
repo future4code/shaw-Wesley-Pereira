339c08bc89a396ab6735e7dc9f5357515df7f60d
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-02-24 15:16:41 -0300

    Exercicios de Funçoes de Array

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,7 @@
+// Exercício de Leitura de Código
+//1.a) Será impresso cada item do objeto, mais o tamanho do array, mais 3 vezes o array.
+//2.a) Será impresso um novo array listando só o nome dos itens do objeto, mais o tamanho do array, mais 3 vezes o array.
+//3.a) Será imprpresso um novo array listando todos os apelidos diferentes de "Chijo", mais o tamanho do array, mais 3 vezes o array.
+
+//Exercício de Escrita de Código
+// 1.a)
