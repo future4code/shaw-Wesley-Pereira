4e3bf8e9844cd96ff5489ebf9121791a2f1898e5
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-03-07 12:27:27 -0300

    Exercicio1

---
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,100 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+   return array.length
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  return array.reverse()
+}
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  const ordem = array.sort((a, b) => a - b);
+  return ordem
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+  const numeroPar = function(numero){
+  return numero % 2 == 0
+  }
+  let pares = array.filter(numeroPar);
+  return pares
+  
+}
+
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+   const numeroParElevado = numero => {
+   return numero % 2 == 0
+ }
+   let paresElevado = array.filter(numeroParElevado)
+   return paresElevado
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  return Math.max.apply(Math, array)
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
