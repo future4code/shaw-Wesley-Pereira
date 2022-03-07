1f4eaaf7eedb44c66305eae0cbd6b96316507ccf
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-03-07 14:19:03 -0300

    Exercicio6

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -40,9 +40,9 @@ function retornaNumerosParesElevadosADois(array) {
 }
 
 
-// // EXERCÍCIO 06
-// function retornaMaiorNumero(array) {
-//   return Math.max.apply(Math, array)
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  return Math.max.apply(Math, array)
 }
 
 // EXERCÍCIO 07
