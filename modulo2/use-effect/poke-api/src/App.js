b42bd599784e2fe332053f0b8630f69a65e6ebe2
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-04-20 09:38:56 -0300

    Problema com useEffect corrigido

--- modulo2/use-effect/poke-api/src/App.js
+++ modulo2/use-effect/poke-api/src/App.js
@@ -5,42 +5,36 @@ import PokeCard from "./components/PokeCard/PokeCard";
 import { useState, useEffect } from "react";
 
 function App (props) {
-  // state = {
-    // lista de pokemons que está sendo guardada no estado
-    // pokeList: [],
+  
     const [pokeList, setPokeList] = useState([]);
-    // nome do pokemon guardado no estado, assim que o usuário
-    // escolhe um nome no dropdown
-    // pokeName: ""
+    
     const [pokeName, setPokeName] = useState("");
-  // };
+  
 
-  // método que roda após a montagem do componente
-  componentDidMount = () => {
-    // função axios que está batendo na API e buscando 151 pokemons
+  
+    useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
       .then(response => {
-        // função que está setando no estado os 151 pokemons
+        
         setPokeList(response.data.results);
       })
       .catch(err => {
         console.log(err);
       });
-  };
+  }, []);
 
-  changePokeName = event => {
+  const changePokeName = event => {
     setPokeName(event.target.value);
   };
 
   
     return (
       <div className="App">
-        {/* evento onChange chama função toda vez que o usuário 
-        escolhe um novo pokemon no dropdown */}
-        <select onChange={event => setPokeName(event.target.value)}>
+        
+        <select onChange={changePokeName}>
           <option value={""}>Nenhum</option>
-          {/* renderizando a lista de pokemons como opções do select */}
+          
           {pokeList.map(pokemon => {
             return (
               <option key={pokemon.name} value={pokemon.name}>
@@ -49,8 +43,7 @@ function App (props) {
             );
           })}
         </select>
-        {/* expressão booleana que renderiza o componente PokeCard,
-        caso o valor de pokeName, no estado, seja true */}
+        
         {pokeName && <PokeCard pokemon={pokeName} />}
       </div>
     );
--- modulo2/use-effect/poke-api/src/components/PokeCard/PokeCard.js
+++ modulo2/use-effect/poke-api/src/components/PokeCard/PokeCard.js
@@ -3,56 +3,39 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
  function PokeCard (props) {
-  // state = {
-    // valor do estado que guarda infos e foto do pokemon
-  //   pokemon: {}
-    const [pokemon, setPokemon] = useState({});
-  // };
+  
+    const [pokemonNome, setPokemonNome] = useState({});
+  
 
-  // método que roda após a montagem do componente
-  // componentDidMount() {
-  //   this.pegaPokemon(this.props.pokemon);
-  // }
+  
+  useEffect(() => {
+    pegaPokemon(props.pokemon);
+  }, [  props.pokemon ]);
 
-  // método que roda após a atualização do componente.
-  // Um dos casos de atualização do componente é a
-  // mudança da props que está sendo passado pra ele
-  // componentDidUpdate(prevProps) {
-    // aqui, é feita uma verificação da props anterior com a props atual.
-    // Caso a props anterior seja diferente da props atual,
-    // a função pegaPokemon é chamada.
-    if (prevProps.pokemon !== this.props.pokemon) {
-      this.pegaPokemon(this.props.pokemon);
-    }
-  }
 
-  // função que bate na poke API com um nome específico de pokemon
-  // Isso permite que consigamos pegar as infos dos pokemons.
-  // Nos métodos de ciclo de vida, ela é chamada passando como
-  // parâmetro o nome de pokemon que está chegando como props.
-  pegaPokemon = pokeName => {
+
+  
+  const pegaPokemon = pokeName => {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
       .then(response => {
-        // guarda as infos do pokemon no estado
-        setPokemon({ pokemon: response.data });
+        
+        setPokemonNome(response.data);
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-  useEffect(())
-
-    const pokemon = pokemon
+  
 
     return (
       <div>
-        <p>{pokemon.name}</p>
-        <p>{pokemon.weight} Kg</p>
-        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
-        {pokemon.sprites && (
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <p>{pokemonNome.name}</p>
+        <p>{pokemonNome.weight ? `${pokemonNome.weight} KG ` :``} </p>
+        {pokemonNome.types && <p>{pokemonNome.types[0].type.name}</p>}
+        {pokemonNome.sprites && (
+          <img src={pokemonNome.sprites.front_default} alt={pokemonNome.name} />
         )}
       </div>
     );
