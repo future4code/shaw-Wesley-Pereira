a323d994f14f1e1383fdf6a0293a3f7679b89dc6
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-05-12 15:03:22 -0300

    Projeto Labeddit Pages

--- modulo2/projeto-labeddit/projeto-labeddit/src/components/LoginButton/LoginButton.js
+++ modulo2/projeto-labeddit/projeto-labeddit/src/components/LoginButton/LoginButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function LoginButton() {
     return (
         <div>
-            
+            <Link to="/login"></Link>
         </div>
     );
 }
