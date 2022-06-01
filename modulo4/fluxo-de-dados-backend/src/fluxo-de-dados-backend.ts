e9e0a82344de19cba5c845596f766b3195ec6d89
Wesley da Silva Pereira <wkdev7@gmail.com>
2022-06-01 16:55:41 -0300

    Feito ate o numero 7

--- modulo4/fluxo-de-dados-backend/src/fluxo-de-dados-backend.ts
+++ modulo4/fluxo-de-dados-backend/src/fluxo-de-dados-backend.ts
@@ -17,27 +17,67 @@ app.get("/products", (req, res) => {
 
 
 app.post("/products", (req, res) => {
+    try{
+    const token = req.headers.authorization;
+    if(!token) {
+        throw new Error("Token não informado");
+    }
+
     const { name, price, image, status } = req.body;
-    const id = products[products.length - 1].id + 1;
-    products.push({ id, name, price, image, status });
-    res.json(products);
+    const id = products.length + 1;
+    const product = { id, name, price, image, status };
+    products.push(product);
+    res.json(product);
+    } catch(error: any) {
+        res.status(400).json({ error: error.message });
+    }
+
 });
 
 
 app.put("/products/:id", (req, res) => {
+    try{
+        const token = req.headers.authorization;
+    if(!token) {
+        throw new Error("Token não informado");
+    }
     const { id } = req.params;
     const { price } = req.body;
     const productIndex = products.findIndex((product: any) => product.id == id);
     products[productIndex].price = price;
     res.json(products);
+    // if(!price) {
+    //     throw new Error("Preço não informado");
+    // }else if(price != Number(price)) {
+    //     throw new Error("Preço inválido");
+    // }else if(price <= 0) {
+    //     throw new Error("Preço inválido");
+    // }else if(productIndex < 0) {
+    //     throw new Error("Produto não encontrado");
+    // }
+}   catch(error: any) {
+    res.status(400).json({ error: error.message });
+
+    }
+
 });
 
 app.delete("/products/:id", (req, res) => {
-    const { id } = req.params;
+    try{
+        const token = req.headers.authorization;
+    if(!token) 
+    {
+        const { id } = req.params;
     const productIndex = products.findIndex((product: any) => product.id == id);
     products.splice(productIndex, 1);
-    res.json(products);
+    res.json(products)
+}
+    }   catch(error: any) {
+        res.status(400).json({ error: error.message });
+    }
 });
+
+
 
 
 app.listen(3004, () => {
